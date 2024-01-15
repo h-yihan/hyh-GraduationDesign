5ca91b46e7e8eeefa77bf139e8b4638bb7f1df90
h-yihan <2876003729@qq.com>
2024-01-16 01:53:50 +0800

    毕业设计-小程序

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,66 +1,45 @@
-// pages/index/index.js
+// index.js
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    ggao: [{
+      id: 1,
+      addGgao1: "喜茶百货",
+      addGgao2: "中秋月饼抢鲜上市"
+    },
+    {
+      id: 2,
+      addGgao1: "阿喜团餐",
+      addGgao2: "企业欢聚享福利"
+    },
+    {
+      id: 3,
+      addGgao1: "阿喜有礼",
+      addGgao2: "初夏豪礼即可送"
+    },
+  ],
+    swiper: [{
+      id: 1,
+      swiper_item: [{
+          id: 1,
+          image: "https://heyihan.codedemo.vip/indexBanner-1.png"
+        }, {
+          id: 2,
+          image: "https://heyihan.codedemo.vip/indexBanner-1.png"
+        }, {
+          id: 3,
+          image: "https://heyihan.codedemo.vip/indexBanner-2.png"
+        },
+        {
+          id: 4,
+          image: "https://heyihan.codedemo.vip/indexBanner-2.png"
+        }
+      ]
+    }]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
+  onYqh(){
+    wx.navigateTo({
+      url: '/pages/yqh_zq/yqh_zq',
+    })
   }
 })
